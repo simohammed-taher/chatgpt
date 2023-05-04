5770e8e7bc286a1be92d70c6db28a5a11042de79
MOHAMMED TAHER <100471051+samhamed@users.noreply.github.com>
2023-05-05 00:07:26 +0100

    05

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-
 import { useEffect, useState } from "react";
 
 function App() {
-  const [value, setValue] = useState("");
+  const [value, setValue] = useState(null);
   const [message, setMessage] = useState(null);
   const [previousChats, setPreviousChats] = useState([]);
   const [currentTitle, setCurrentTitle] = useState(null);
 
-  const createNewChat = () => {
+  const creatNewChat = () => {
     setMessage(null);
     setValue("");
     setCurrentTitle(null);
   };
 
-  const handleClick = (title) => {
-    setCurrentTitle(title);
+  const handleClick = (uniqueTitle) => {
+    setCurrentTitle(uniqueTitle);
     setMessage(null);
     setValue("");
   };
@@ -22,34 +21,31 @@ function App() {
   const getMessages = async () => {
     const options = {
       method: "POST",
-      body: JSON.stringify({ message: value }),
+      body: JSON.stringify({
+        message: value,
+      }),
       headers: {
         "Content-Type": "application/json",
       },
     };
     try {
-      setValue("");
       const response = await fetch("http://localhost:8000/completions", options);
-
       const data = await response.json();
-
-      if (data.choices && data.choices[0] && data.choices[0].message) {
-        setMessage(data.choices[0].message);
-      } else {
-        console.error("Unexpected response:", data);
-      }
+      setMessage(data.choices[0].message);
     } catch (error) {
       console.error(error);
     }
   };
 
+
   useEffect(() => {
+    console.log(currentTitle, value, message);
     if (!currentTitle && value && message) {
       setCurrentTitle(value);
     }
     if (currentTitle && value && message) {
-      setPreviousChats((prevChats) => [
-        ...prevChats,
+      setPreviousChats(prevChats => (
+        [...previousChats,
         {
           title: currentTitle,
           role: "user",
@@ -60,56 +56,59 @@ function App() {
           role: message.role,
           content: message.content,
         },
-      ]);
+        ]
+      ))
     }
-  }, [message]);
+  }, [message, currentTitle]);
 
-  const currentChat = previousChats.filter(
-    (chat) => chat.title === currentTitle
-  );
-  const uniqueTitles = Array.from(
-    new Set(previousChats.map((chat) => chat.title))
-  );
+  const currentChat = previousChats.filter(previousChats => previousChats.title === currentTitle);
+  const uniqueTitles = Array.from(new Set(previousChats.map((previousChats) => previousChats.title)));
 
+  console.log(uniqueTitles);
   return (
     <div className="app">
       <section className="side-bar">
-        <button onClick={createNewChat} className="btnchat">+ New chat</button>
+        <button onClick={creatNewChat}>+ New chat</button>
         <ul className="history">
-          {uniqueTitles && uniqueTitles.map((title, index) => (
-            <li key={index} onClick={() => handleClick(title)}>
-              {title}
+          {uniqueTitles?.map((uniqueTitle, index) =>
+            <li key={index} onClick={() => handleClick(uniqueTitle)}>
+              {uniqueTitle}
             </li>
-          ))}
+          )}
         </ul>
         <nav>
-          <p>Made by XM01</p>
+          <p>Made by Taher</p>
         </nav>
       </section>
       <section className="main">
-        {!currentTitle && <h1>Prodware-GPT</h1>}
+        {!currentTitle && <h1>OFPPT-GPT-4</h1>}
         <ul className="feed">
-          {currentChat && currentChat.map((chatMessage, index) => (
+          {currentChat?.map((chatMessage, index) =>
             <li key={index}>
               <p className="role">{chatMessage.role}</p>
               <p>{chatMessage.content}</p>
             </li>
-          ))}
+          )}
         </ul>
         <div className="bottom-section">
           <div className="input-container">
-            <input
-              value={value}
-              placeholder="Send a message"
-              onChange={(e) => setValue(e.target.value)}
+            <input value={value} placeholder="Send a message" onChange={(e) => setValue(e.target.value)}
+              onKeyDown={(e) => {
+                e.keyCode === 13 && e.shiftKey === false && getMessages();
+              }}
             />
             <div id="submit" onClick={getMessages} className="subb">
-              ➢
+              {/* ➢ */}
+              <img
+                src="./send.png"
+                width={15}
+                alt="send-button"
+                className="absolute top-4 right-3 hover:cursor-pointer ease-in duration-100 hover:scale-125"
+              />
             </div>
           </div>
           <p className="info">
-            ChatGPT may produce inaccurate information about people, places, or
-            facts. ChatGPT Mar 23 Version
+            ChatGPT may produce inaccurate information about people, places, or facts. ChatGPT Mar 23 Version
           </p>
         </div>
       </section>
@@ -118,4 +117,5 @@ function App() {
 }
 
 export default App;
+
 
